4f6fa1d4d0e9e7256c4519f6fcfe382fab641f2a
nishi <nishic@shriffle.com>
2024-02-01 12:51:54 +0530

    added functionality for creating twitter post and some ui changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,47 +1,22 @@
-# app/controllers/posts_controller.rb
 class PostsController < ApplicationController
-  before_action :authenticate_user!
+    before_action :authenticate_user!
 
-  def new
-    @post = Post.new
-  end
+    def create
+	    @post = current_user.posts.new(content: params[:content])
 
-  def create
-    @post = current_user.posts.new(post_params)
+	    if @post.save
+	      TwitterService.post_tweet(params[:content], current_user)
 
-    if @post.save
-      share_on_twitter(@post.content)
-
-      redirect_to posts_path, notice: 'Post was successfully created.'
-    else
-      render :new
+	      redirect_to root_path, notice: 'Post was successfully created.'
+	    else
+	      render :new
+	    end
     end
-  end
+ end
 
-  def index
-    @posts = Post.all
-  end
 
-  private
 
-  def post_params
-    params.require(:post).permit(:content)
-  end
 
-  def share_on_twitter(content)
-    client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = 'gaq6ueTgSyyU7d9RwgJoazdbg'
-      config.consumer_secret     = 'LKiz4xCRhH2jeckWfoVlZahSGkCQAetm0Kux9XQHVj9Hx5fbEA'
-      config.access_token        = '1752230110319513600-nY5kXq5vvPXpV5SXphUVbjQxTx56dS'
-      config.access_token_secret = 'dtixmMckgcgWPAmy93NZmAGXv8XWsN6Ttsfzqtb4Q2ZJX'
-    end
 
-    tweet = "New post: #{content}"
-    begin
-      client.update(tweet)
-    rescue Twitter::Error => e
-      # Handle errors (e.g., log them, show a flash message, etc.)
-      flash[:alert] = "Error sharing on Twitter: #{e.message}"
-    end
-  end
-end
+
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,41 +4,19 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: [:twitter, :facebook]
   has_many :posts       
 
-  # def self.from_omniauth(auth)
-  #   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-  #     user.email = auth.info.email
-  #     user.password = Devise.friendly_token[0, 20]
-  #   end
-  # end
-
-  # def self.from_omniauth(auth)
-  #   byebug
-  #   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-  #   user.email = auth.info.email
-  #   user.password = Devise.friendly_token[0, 20]
-  #   # user.name = auth.info.name # assuming the user model has a name
-  #   # user.username = auth.info.nickname # assuming the user model has a username
-  #   #user.image = auth.info.image # assuming the user model has an image
-  #   # If you are using confirmable and the provider(s) you use validate emails,
-  #   # uncomment the line below to skip the confirmation emails.
-  #   # user.skip_confirmation!
-  #   end
-  # end
-
-  # def self.from_omniauth(auth)
-  #     name_split = auth.info.name.split(" ")
-  #     user = User.where(email: auth.info.email).first
-  #     user ||= User.create!(provider: auth.provider, uid: auth.uid, email: auth.info.email || 'nishic.shriffle@gmail.com', password: Devise.friendly_token[0, 20])
-  #       user
-  # end
-
   def self.from_omniauth(auth)
     Rails.logger.debug("Omniauth data: #{auth.inspect}")
 
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       Rails.logger.debug("User attributes: #{user.attributes}")
       user.email = auth.info.email
+      user.access_token =  auth.credentials.token
+      user.consumer_secret_key =  auth.extra.access_token.consumer.secret
+      user.consumer_key =  auth.extra.access_token.consumer.key
+      user.access_secret_token =  auth.credentials.secret
       user.password = Devise.friendly_token[0, 20]
     end
   end
 end
+
+
